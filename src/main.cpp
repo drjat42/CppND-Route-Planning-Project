52056a9f7bf5d55b966189f5324eb5154d6b91c9
Joshua A. Tauber <drjat42@yahoo.com>
2021-09-09 13:57:20 +0000

    Implement OMP A* search for project submission.

--- src/main.cpp
+++ src/main.cpp
@@ -51,16 +51,20 @@ int main(int argc, const char **argv)
         else
             osm_data = std::move(*data);
     }
-    
-    // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
-    // user input for these values using std::cin. Pass the user input to the
-    // RoutePlanner object below in place of 10, 10, 90, 90.
+
+    char c;
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter starting x, y coordinates: ";
+    std::cin >> start_x >> c >> start_y;
+    std::cout << "Enter target x, y coordinates: ";
+    std::cin >> end_x >> c >> end_y;
+    std::cout << "Searching for a path from (" << start_x << ", " << start_y << ") to (" << end_x << ", " << end_y << ")" << std::endl;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
